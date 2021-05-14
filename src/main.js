25d4f401de3b3d15151526f456efea4fc4597e48
fubai <lidongw7@gmail.com>
2021-05-14 21:15:06 +0800

    feat:按需引入element组件

--- src/main.js
+++ src/main.js
@@ -1,22 +1,92 @@
 import Vue from 'vue'
+import {
+  Button,
+  Link,
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Form,
+  FormItem,
+  Input,
+  InputNumber,
+  Select,
+  Option,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  DatePicker,
+  Table,
+  TableColumn,
+  Pagination,
+  Popover,
+  Card,
+  Upload,
+  Tree,
+  Drawer,
+  Tag,
+  Row,
+  Col,
+  Loading,
+  Message,
+  MessageBox
+} from 'element-ui'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import AjaxPlugin from './plugins/ajax'
 import BizPlugin from './plugins/biz'
 import App from './page/App'
 import Login from './page/Login'
 import StudentTrainDrawer from './page/train/StudentTrainDrawer'
+import 'element-ui/lib/theme-chalk/index.css'
 import './scss/element-variables.scss'
 
 require('es6-promise').polyfill()
 
-Vue.use(ElementUI)
+Vue.component(Button.name, Button)
+Vue.component(Link.name, Link)
+Vue.component(Dialog.name, Dialog)
+Vue.component(Menu.name, Menu)
+Vue.component(Submenu.name, Submenu)
+Vue.component(MenuItem.name, MenuItem)
+Vue.component(Dropdown.name, Dropdown)
+Vue.component(DropdownMenu.name, DropdownMenu)
+Vue.component(DropdownItem.name, DropdownItem)
+Vue.component(Form.name, Form)
+Vue.component(FormItem.name, FormItem)
+Vue.component(Input.name, Input)
+Vue.component(InputNumber.name, InputNumber)
+Vue.component(Select.name, Select)
+Vue.component(Option.name, Option)
+Vue.component(Radio.name, Radio)
+Vue.component(RadioGroup.name, RadioGroup)
+Vue.component(RadioButton.name, RadioButton)
+Vue.component(DatePicker.name, DatePicker)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
+Vue.component(Pagination.name, Pagination)
+Vue.component(Popover.name, Popover)
+Vue.component(Card.name, Card)
+Vue.component(Upload.name, Upload)
+Vue.component(Tree.name, Tree)
+Vue.component(Drawer.name, Drawer)
+Vue.component(Tag.name, Tag)
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.use(Loading.directive)
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$message = Message
 Vue.use(AjaxPlugin)
 Vue.use(BizPlugin)
-Vue.component('student-train-drawer', StudentTrainDrawer)
 Vue.config.productionTip = false
+Vue.component('student-train-drawer', StudentTrainDrawer)
 
 
 let adminJSON = sessionStorage.getItem('admin')
